be030344e6b05d080a7316b2a9cbf510793e48ab
Lana <lanadzuceva@gmail.com>
2023-03-30 16:43:20 +0300

    Test commit

--- ScheduleEditor/ClassScheduleError.cs
+++ ScheduleEditor/ClassScheduleError.cs
@@ -14,6 +14,7 @@ namespace SheduleEditorV6
     public class ScheduleError
     {
         public string GroupTitle { get; set; }
+        //test
         public SAcademicClass ScheduleAcademicClass { get; set; }
         public string Message { get; set; }
     }
