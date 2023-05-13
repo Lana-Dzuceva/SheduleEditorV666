13d91f05ea8ec5056af422d67d5a785d6b0ddbbe
Lana <lanadzuceva@gmail.com>
2023-05-13 21:36:36 +0300

    Исправила функцию IsCellAvaible

--- ScheduleEditorClassLibrary/Schedule.cs
+++ ScheduleEditorClassLibrary/Schedule.cs
@@ -34,17 +34,18 @@ namespace ScheduleEditorClassLibrary
                 return Results.TypeMismatch;
             for (int i = 0; i < Groups.Count; i++)
             {
-                var group = Groups[i][(DayOfWeek)(row / 8 + 1), (row - row / 8 * 8) / 2 + 1];
-                if (group == null) continue;
-                bool simple = academicClass.Type == ClassTypes.Lecture && academicClass.Hours > 36 && group.Group1week1.Teacher == academicClass.Teacher;
-                bool twoWeeks1 = academicClass.Type == ClassTypes.Lecture && academicClass.Hours <= 36 && row % 2 == 0 && group.Group1week1.Teacher == academicClass.Teacher;
-                bool twoWeeks2 = academicClass.Type == ClassTypes.Lecture && academicClass.Hours <= 36 && row % 2 != 0 && group.Group1week2.Teacher == academicClass.Teacher;
-                bool twoGroups1 = academicClass.Type == ClassTypes.Practice && academicClass.Hours > 36 && col < 2 && group.Group1week1.Teacher == academicClass.Teacher;
-                bool twoGroups2 = academicClass.Type == ClassTypes.Practice && academicClass.Hours > 36 && col > 2 && group.Group2week1.Teacher == academicClass.Teacher;
-                bool twoGroupsAndTwoWeeks1 = academicClass.Type == ClassTypes.Practice && academicClass.Hours <= 36 && col < 2 && row % 2 == 0 && group.Group1week1.Teacher == academicClass.Teacher;
-                bool twoGroupsAndTwoWeeks2 = academicClass.Type == ClassTypes.Practice && academicClass.Hours <= 36 && col < 2 && row % 2 != 0 && group.Group1week2.Teacher == academicClass.Teacher;
-                bool twoGroupsAndTwoWeeks3 = academicClass.Type == ClassTypes.Practice && academicClass.Hours <= 36 && col >= 2 && row % 2 == 0 && group.Group2week1.Teacher == academicClass.Teacher;
-                bool twoGroupsAndTwoWeeks4 = academicClass.Type == ClassTypes.Practice && academicClass.Hours <= 36 && col >= 2 && row % 2 != 0 && group.Group2week2.Teacher == academicClass.Teacher;
+                if (Groups[i].Title == activeGroup) continue;
+                var sRow = Groups[i][(DayOfWeek)(row / 8 + 1), (row - row / 8 * 8) / 2 + 1];
+                if (sRow == null) continue;
+                bool simple = academicClass.Type == ClassTypes.Lecture && academicClass.Hours > 36 && sRow.Group1week1.Teacher == academicClass.Teacher;
+                bool twoWeeks1 = academicClass.Type == ClassTypes.Lecture && academicClass.Hours <= 36 && row % 2 == 0 && sRow.Group1week1.Teacher == academicClass.Teacher;
+                bool twoWeeks2 = academicClass.Type == ClassTypes.Lecture && academicClass.Hours <= 36 && row % 2 != 0 && sRow.Group1week2.Teacher == academicClass.Teacher;
+                bool twoGroups1 = academicClass.Type == ClassTypes.Practice && academicClass.Hours > 36 && col < 2 && sRow.Group1week1.Teacher == academicClass.Teacher;
+                bool twoGroups2 = academicClass.Type == ClassTypes.Practice && academicClass.Hours > 36 && col > 2 && sRow.Group2week1.Teacher == academicClass.Teacher;
+                bool twoGroupsAndTwoWeeks1 = academicClass.Type == ClassTypes.Practice && academicClass.Hours <= 36 && col < 2 && row % 2 == 0 && sRow.Group1week1.Teacher == academicClass.Teacher;
+                bool twoGroupsAndTwoWeeks2 = academicClass.Type == ClassTypes.Practice && academicClass.Hours <= 36 && col < 2 && row % 2 != 0 && sRow.Group1week2.Teacher == academicClass.Teacher;
+                bool twoGroupsAndTwoWeeks3 = academicClass.Type == ClassTypes.Practice && academicClass.Hours <= 36 && col >= 2 && row % 2 == 0 && sRow.Group2week1.Teacher == academicClass.Teacher;
+                bool twoGroupsAndTwoWeeks4 = academicClass.Type == ClassTypes.Practice && academicClass.Hours <= 36 && col >= 2 && row % 2 != 0 && sRow.Group2week2.Teacher == academicClass.Teacher;
 
                 if (simple || twoWeeks1 || twoWeeks2 || twoGroups1 || twoGroups2 || twoGroupsAndTwoWeeks1 || twoGroupsAndTwoWeeks2 || twoGroupsAndTwoWeeks3 || twoGroupsAndTwoWeeks4)
                 {
--- ScheduleEditorClassLibrary/Teacher.cs
+++ ScheduleEditorClassLibrary/Teacher.cs
@@ -19,5 +19,27 @@ namespace ScheduleEditorClassLibrary
         {
             return Name;
         }
+        public static bool operator ==(Teacher teacher1, Teacher teacher2)
+        {
+            if (object.ReferenceEquals(teacher1, teacher2))
+            {
+                return true;
+            }
+
+            if (object.ReferenceEquals(teacher1, null) || object.ReferenceEquals(teacher2, null))
+            {
+                return false;
+            }
+
+            return teacher1.Name == teacher2.Name;
+        }
+
+        public static bool operator !=(Teacher person1, Teacher person2)
+        {
+            return !(person1 == person2);
+        }
+   
+
+
     }
 }
