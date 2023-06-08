80e30a2d74a5adb16c23ca01dde89e3bb100e4ea
DonBravisti <butov.s-butov@yandex.ru>
2023-06-08 20:26:47 +0300

    Внес правки в метод SendToDB

--- ScheduleEditorClassLibrary/FacultyGroups.cs
+++ ScheduleEditorClassLibrary/FacultyGroups.cs
@@ -70,22 +70,22 @@ namespace ScheduleEditorClassLibrary
             return result;
         }
 
-        public void SendToDB(string connectionString)
+        public void SendToDB(string connectionString, string databaseName)
         {
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
 
             foreach (var group in Groups)
             {
-                int groupId = GetGroupId(group.Title, connection);
+                int groupId = GetGroupId(group.Title, databaseName, connection);
 
                 foreach (var @class in group.Classes)
                 {
-                    int employeeId = GetEmployeeId(@class.Teacher.Name, connection);
+                    int employeeId = GetEmployeeId(@class.Teacher.Name, databaseName, connection);
                     int subgroup = (int)@class.SubGroup + 1;
                     int type = (int)@class.Type + 1;
 
-                    string query = "INSERT INTO `test`.`schedule` " +
+                    string query = $"INSERT INTO `{databaseName}`.`schedule` " +
                         "(`group_id`, `subgroup_num`, `subject_form_id`, `employee_id`)" +
                         $"VALUES ('{groupId}', '{subgroup}', '{type}', '{employeeId}');";
                     MySqlCommand command = new MySqlCommand(query, connection);
@@ -96,17 +96,17 @@ namespace ScheduleEditorClassLibrary
             connection.Close();
         }
 
-        private int GetEmployeeId(string name, MySqlConnection connection)
+        private int GetEmployeeId(string name, string databaseName, MySqlConnection connection)
         {
             string surname = name.Split()[0];
-            string queryGetGroupId = "SELECT id FROM test.employees WHERE surname='" + surname + "' LIMIT 1";
+            string queryGetGroupId = $"SELECT id FROM {databaseName}.employees WHERE surname='{surname}' LIMIT 1";
             MySqlCommand command = new MySqlCommand(queryGetGroupId, connection);
             return (int)command.ExecuteScalar();
         }
 
-        private int GetGroupId(string title, MySqlConnection connection)
+        private int GetGroupId(string title, string databaseName, MySqlConnection connection)
         {
-            string queryGetGroupId = "SELECT id FROM test.groups WHERE title='" + title + "' LIMIT 1";
+            string queryGetGroupId = $"SELECT id FROM {databaseName}.groups WHERE title='{title}' LIMIT 1";
             MySqlCommand command = new MySqlCommand(queryGetGroupId, connection);
             return (int)command.ExecuteScalar();
         }
