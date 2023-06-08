68d2366ccf84e58f4ef8e11566cf0f24bd31a534
DonBravisti <butov.s-butov@yandex.ru>
2023-06-08 18:57:35 +0300

    Добавил в класс FacultyGroups метод Fill и объвил метод FillGroups

--- ScheduleEditorClassLibrary/FacultyGroups.cs
+++ ScheduleEditorClassLibrary/FacultyGroups.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -14,14 +16,51 @@ namespace ScheduleEditorClassLibrary
         {
             Groups = new List<Group>();
         }
+
         [JsonConstructor]
         public FacultyGroups(List<Group> groups)
         {
             Groups = groups;
         }
+
         public void Add(Group group)
         {
             Groups.Add(group);
+        }
+
+        public void Fill(string connectionString)
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder(connectionString);
+            string databaseName = builder.Database;
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                Groups = FillGroupes(connection, databaseName);
+                //foreach (var group in Groups)
+                //{
+                //    group.FillAcademicClasses(connection);
+                //}
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private List<Group> FillGroupes(MySqlConnection connection, string databaseName)
+        {
+            string query = "";
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(query, connection);
+            DataSet dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+
+            return new List<Group>();
         }
     }
 }
