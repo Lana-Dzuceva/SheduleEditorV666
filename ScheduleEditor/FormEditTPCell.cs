26d2f45e392ec4127bb601c4b69240009b398316
Lana <lanadzuceva@gmail.com>
2023-06-09 21:02:06 +0300

    Revert "абсолютно никому не нужный коммит"
    This reverts commit b53ddcf45f7c20220a91c2eba8137967bc766fc4.

--- ScheduleEditor/FormEditTPCell.cs
+++ ScheduleEditor/FormEditTPCell.cs
@@ -13,20 +13,27 @@ namespace SheduleEditorV6
 {
     public partial class FormEditTPCell : Form
     {
-
-        public List<Teacher> teachersIn, teachersAll;
+        public List<string> cellData;
+        List<string> teachers;
+        DataGridView dataGrid;
+        int row;
+        int col;
         FormTeacherPreferences form;
-        public FormEditTPCell(List<Teacher> teachersIn_, List<Teacher> teachersAll_, FormTeacherPreferences form)
+        public FormEditTPCell(List<string> teachers_, DataGridView dataGrid_, int row_, int col_, FormTeacherPreferences form)
         {
             InitializeComponent();
-            
+            cellData = dataGrid_[col_, row_].Tag as List<string>;
+            teachers = teachers_;
+            dataGrid = dataGrid_;
+            row = row_;
+            col = col_;
             this.form = form;
         }
 
         private void FormEditTPCell_Load(object sender, EventArgs e)
         {
-            listViewIn.Items.AddRange(teachersIn.Select(teacher => new ListViewItem(teacher.Name)).ToArray());
-            listViewOut.Items.AddRange(teachersAll.Where(teacher => !teachersIn.Contains(teacher)).Select(teacher => new ListViewItem(teacher.Name)).ToArray());
+            listViewIn.Items.AddRange(cellData.Select(name => new ListViewItem(name)).ToArray());
+            listViewOut.Items.AddRange(teachers.Where(teacher => !cellData.Contains(teacher)).Select(teacher => new ListViewItem(teacher)).ToArray());
         }
 
 
@@ -38,12 +45,7 @@ namespace SheduleEditorV6
                 var lvi = lv.GetItemAt(e.X, e.Y);
                 var a = lv.DoDragDrop(lvi.Text, DragDropEffects.Move);
                 if (a != DragDropEffects.None)
-                {
                     lv.Items.Remove(lvi);
-                    teachersIn
-                }
-
-                    
             }
             catch (Exception)
             { }
@@ -66,6 +68,10 @@ namespace SheduleEditorV6
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
+            dataGrid[col, row].Tag = listViewIn.Items
+                                        .Cast<ListViewItem>()
+                                        .Select(item => (item as ListViewItem).Text)
+                                        .ToList();
             form.UpdateDGV();
             form.Save();
             //шатала я это все
