fff5fd083b2789793bc1a1609c1dd455ecaf5826
DonBravisti <butov.s-butov@yandex.ru>
2023-06-21 15:07:00 +0300

    Доработал загрузку данных из базы. Убрал из класса FacultyGroups метод SendToDB. Надо добавить его в Schedule.

--- ScheduleEditorClassLibrary/FacultyGroups.cs
+++ ScheduleEditorClassLibrary/FacultyGroups.cs
@@ -37,7 +37,7 @@ namespace ScheduleEditorClassLibrary
             try
             {
                 connection.Open();
-                Groups = FillGroupes(connection, databaseName);
+                FillGroupes(connection, databaseName);
             }
             catch (MySqlException ex)
             {
@@ -49,14 +49,12 @@ namespace ScheduleEditorClassLibrary
             }
         }
 
-        private List<Group> FillGroupes(MySqlConnection connection, string databaseName)
+        private void FillGroupes(MySqlConnection connection, string databaseName)
         {
             string queryGetSchedule = $"SELECT * FROM {databaseName}.groups";
             MySqlDataAdapter dataAdapter = new MySqlDataAdapter(queryGetSchedule, connection);
             DataSet dataSetSchedule = new DataSet();
             dataAdapter.Fill(dataSetSchedule);
-
-            List<Group> result = new List<Group>();
 
             DataTable tableSchedule = dataSetSchedule.Tables[0];
             foreach (DataRow row in tableSchedule.Rows)
@@ -64,50 +62,11 @@ namespace ScheduleEditorClassLibrary
                 string groupTitle = row[1].ToString();
                 Group group = new Group(groupTitle);
                 group.FillAcademicClasses(connection, databaseName);
-                result.Add(group);
+                Groups.Add(group);
             }
-
-            return result;
         }
 
-        public void SendToDB(string connectionString)
-        {
-            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder(connectionString);
-            string databaseName = builder.Database;
-
-            MySqlConnection connection = new MySqlConnection(connectionString);
-            connection.Open();
-
-            foreach (var group in Groups)
-            {
-                int groupId = GetGroupId(group.Title, databaseName, connection);
-
-                foreach (var @class in group.Classes)
-                {
-                    int employeeId = GetEmployeeId(@class.Teacher.Name, databaseName, connection);
-                    int subgroup = (int)@class.SubGroup + 1;
-                    int type = (int)@class.Type + 1;
-
-                    string query = $"INSERT INTO `{databaseName}`.`schedule` " +
-                        "(`group_id`, `subgroup_num`, `subject_form_id`, `employee_id`)" +
-                        $"VALUES ('{groupId}', '{subgroup}', '{type}', '{employeeId}');";
-                    MySqlCommand command = new MySqlCommand(query, connection);
-                    command.ExecuteNonQuery();
-                }
-            }
-
-            connection.Close();
-        }
-
-        private int GetEmployeeId(string name, string databaseName, MySqlConnection connection)
-        {
-            string surname = name.Split()[0];
-            string queryGetGroupId = $"SELECT id FROM {databaseName}.employees WHERE surname='{surname}' LIMIT 1";
-            MySqlCommand command = new MySqlCommand(queryGetGroupId, connection);
-            return (int)command.ExecuteScalar();
-        }
-
-        private int GetGroupId(string title, string databaseName, MySqlConnection connection)
+        internal static int GetGroupId(string title, string databaseName, MySqlConnection connection)
         {
             string queryGetGroupId = $"SELECT id FROM {databaseName}.groups WHERE title='{title}' LIMIT 1";
             MySqlCommand command = new MySqlCommand(queryGetGroupId, connection);
